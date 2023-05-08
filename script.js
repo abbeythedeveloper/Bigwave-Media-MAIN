37398f6919fa5f86f25150f1ef396a268b1fa1f3
AbbeyTK <ogundiranabbey057@gmail.com>
2023-05-08 06:24:02 -0700

    hehe

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const body = document.querySelector("body");
+const h2 = document.querySelector("h2");
+const h6 = document.querySelector("h6");
 const toggle = document.querySelector("#toggle");
 const sunIcon = document.querySelector(".toggle .bxs-sun");
 const moonIcon = document.querySelector(".toggle .bx-moon");
@@ -6,6 +8,8 @@ const moonIcon = document.querySelector(".toggle .bx-moon");
 toggle.addEventListener("change", () => {
     
     body.classList.toggle("dark");
+    h2.classList.toggle("darktext");
+    h6.classList.toggle("darktexts");
     sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
     moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
 
